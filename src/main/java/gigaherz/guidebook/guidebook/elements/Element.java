a7fcd52d10a62ccc5c8f34ac5fb45a8def7f9794
David Quintana <gigaherz@gmail.com>
2020-07-05 02:41:31 +0200

    Uncommited stuff.

--- src/main/java/gigaherz/guidebook/guidebook/elements/Element.java
+++ src/main/java/gigaherz/guidebook/guidebook/elements/Element.java
@@ -1,5 +1,6 @@
 package gigaherz.guidebook.guidebook.elements;
 
+import gigaherz.guidebook.GuidebookMod;
 import gigaherz.guidebook.guidebook.IBookGraphics;
 import gigaherz.guidebook.guidebook.IConditionSource;
 import gigaherz.guidebook.guidebook.conditions.ConditionContext;
@@ -15,6 +16,8 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 import java.util.function.Predicate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public abstract class Element
 {
@@ -252,30 +255,64 @@ public abstract class Element
         return def;
     }
 
-    protected static int getColorAttribute(NamedNodeMap attributes, String name, int def)
+    // For an exploded description of this regex, see: https://regex101.com/r/qXfiDc/1/
+    private static final Pattern COLOR_PARSE = Pattern.compile("^#?(?:(?<c3>[0-9a-f]{3})|(?<c6>[0-9a-f]{6})|(?<c8>[0-9a-f]{8})|(?<rgb>rgb\\((?<r>[0-9]{1,3}),(?<g>[0-9]{1,3}),(?<b>[0-9]{1,3})\\))|(?<rgba>rgba\\((?<r2>[0-9]{1,3}),(?<g2>[0-9]{1,3}),(?<b2>[0-9]{1,3}),(?<a2>[0-9]{1,3})\\)))$");
+    protected static int getColorAttribute(NamedNodeMap attributes, int def)
     {
         Node attr = attributes.getNamedItem("color");
         if (attr != null)
         {
             String c = attr.getTextContent();
 
-            if (c.startsWith("#"))
-                c = c.substring(1);
+            Matcher m = COLOR_PARSE.matcher(c);
+            if (m.matches())
+            {
+                try
+                {
+                    String value;
+                    if ((value = m.group("c3")) != null)
+                    {
+                        String s = new String(new char[]{value.charAt(0),value.charAt(0),value.charAt(1),value.charAt(1),value.charAt(2),value.charAt(2)});
+                        return 0xFF000000 | Integer.parseInt(s, 16);
+                    }
+                    else if ((value = m.group("c6")) != null)
+                    {
+                        return 0xFF000000 | Integer.parseInt(value, 16);
+                    }
+                    else if ((value = m.group("c8")) != null)
+                    {
+                        return Integer.parseUnsignedInt(value, 16);
+                    }
+                    else if (m.group("rgb") != null)
+                    {
+                        int r = Integer.parseUnsignedInt(m.group("r"));
+                        int g = Integer.parseUnsignedInt(m.group("g"));
+                        int b = Integer.parseUnsignedInt(m.group("b"));
+                        if (r > 255) throw new NumberFormatException("Number too big. Expected range: 0 to 255");
+                        if (g > 255) throw new NumberFormatException("Number too big. Expected range: 0 to 255");
+                        if (b > 255) throw new NumberFormatException("Number too big. Expected range: 0 to 255");
+                        return 0xFF000000 | (r << 16) | (g << 8) | b;
+                    }
+                    else if(m.group("rgba") != null)
+                    {
+                        int r = Integer.parseUnsignedInt(m.group("r2"));
+                        int g = Integer.parseUnsignedInt(m.group("g2"));
+                        int b = Integer.parseUnsignedInt(m.group("b2"));
+                        int a = Integer.parseUnsignedInt(m.group("a2"));
+                        if (r > 255) throw new NumberFormatException("Number too big. Expected range: 0 to 255");
+                        if (g > 255) throw new NumberFormatException("Number too big. Expected range: 0 to 255");
+                        if (b > 255) throw new NumberFormatException("Number too big. Expected range: 0 to 255");
+                        if (a > 255) throw new NumberFormatException("Number too big. Expected range: 0 to 255");
+                        return (a << 24) | (r << 16) | (g << 8) | b;
+                    }
 
-            try
-            {
-                if (c.length() <= 6)
-                {
-                    return 0xFF000000 | Integer.parseInt(c, 16);
+                    GuidebookMod.logger.warn("Unrecognized color value {}, ignored.", c);
                 }
-                else
+                catch (NumberFormatException e)
                 {
-                    return Integer.parseInt(c, 16);
+                    GuidebookMod.logger.warn("Color value not valid {}", c, e);
+                    // ignored
                 }
-            }
-            catch (NumberFormatException e)
-            {
-                // ignored
             }
         }
 
