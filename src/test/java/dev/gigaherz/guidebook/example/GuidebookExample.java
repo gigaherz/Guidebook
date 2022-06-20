f10308449ae3fc6bdac7859e47432cae9ef2fe16
Minecraftschurli <minecraftschurli@gmail.com>
2022-06-20 19:48:13 +0200

    add datagen and test

--- src/test/java/dev/gigaherz/guidebook/example/GuidebookExample.java
+++ src/test/java/dev/gigaherz/guidebook/example/GuidebookExample.java
@@ -1,6 +1,8 @@
 package dev.gigaherz.guidebook.example;
 
+import dev.gigaherz.guidebook.data.BookProvider;
 import dev.gigaherz.guidebook.guidebook.GuidebookItem;
+import net.minecraft.data.DataGenerator;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
@@ -8,10 +10,14 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.common.util.Lazy;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.forge.event.lifecycle.GatherDataEvent;
 import net.minecraftforge.registries.RegistryObject;
 import net.minecraftforge.items.ItemHandlerHelper;
 import net.minecraftforge.registries.ForgeRegistries;
 
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, modid = "gbook")
 public class GuidebookExample
 {
     public static final RegistryObject<GuidebookItem> GUIDEBOOK_ITEM = RegistryObject.create(new ResourceLocation("gbook","guidebook"), ForgeRegistries.ITEMS);
@@ -24,6 +30,31 @@ public class GuidebookExample
 
     public void test()
     {
+    }
+
+    @SubscribeEvent
+    public static void testDatagen(GatherDataEvent event)
+    {
+        DataGenerator generator = event.getGenerator();
+        generator.addProvider(event.includeClient(), new BookProvider(generator, event.getExistingFileHelper(), "gbooktest") {
+
+            @Override
+            public String getName()
+            {
+                return "Guidebook Test";
+            }
+
+            @Override
+            protected void createBooks()
+            {
+                book("testbook")
+                        .chapter("testchapter1", chapterBuilder -> chapterBuilder.page("testpage1", pageBuilder -> pageBuilder.text("testtext1")))
+                        .chapter("testchapter2", chapterBuilder -> {});
+                book("extracted-testbook", true, true, true)
+                        .chapter("testchapter1", chapterBuilder -> chapterBuilder.page("testpage1", pageBuilder -> pageBuilder.text("testtext1")))
+                        .chapter("testchapter2", chapterBuilder -> {});
+            }
+        });
     }
 
     public static void checkGbookGiven(EntityJoinWorldEvent event)
